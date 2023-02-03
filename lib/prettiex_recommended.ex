44f14e32db8a379ba9fbe36f52e92e5daccd53ab
Vicente Merlo <vicente.merlo@thescore.com>
2023-02-03 11:37:46 -0500

    Initial commit

---
+++ lib/prettiex_recommended.ex
@@ -0,0 +1,18 @@
+defmodule PrettiexRecommended do
+  @moduledoc """
+  Documentation for `PrettiexRecommended`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PrettiexRecommended.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/prettiex_recommended_test.exs
@@ -0,0 +1,8 @@
+defmodule PrettiexRecommendedTest do
+  use ExUnit.Case
+  doctest PrettiexRecommended
+
+  test "greets the world" do
+    assert PrettiexRecommended.hello() == :world
+  end
+end
