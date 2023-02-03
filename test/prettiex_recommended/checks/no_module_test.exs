2a72e3fe1ca2dd1d5af873ebe495f8c9b46f67d1
Renan Vaz <renan.vaz@thescore.com>
2023-02-03 18:04:15 -0300

    Add more tests

--- test/prettiex_recommended/checks/no_module_test.exs
+++ test/prettiex_recommended/checks/no_module_test.exs
@@ -18,4 +18,36 @@ defmodule PrettiexRecommended.Checks.NoModuleTest do
 
     assert [%Issue{}] = NoModule.run_on_module(ast)
   end
+
+  test "skip __MODULE__ aliases" do
+    ast =
+      quote do
+        defmodule Person do
+          alias __MODULE__
+          defstruct [:name]
+
+          def build(name) do
+            %{name: name}
+          end
+        end
+      end
+
+    assert [] = NoModule.run_on_module(ast)
+  end
+
+  test "returns issue when both __MODULE__ usage and alias are present" do
+    ast =
+      quote do
+        defmodule Person do
+          alias __MODULE__
+          defstruct [:name]
+
+          def build(name) do
+            %{name: name}
+          end
+        end
+      end
+
+    assert [] = NoModule.run_on_module(ast)
+  end
 end
